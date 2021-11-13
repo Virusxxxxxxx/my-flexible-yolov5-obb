de8cfa52191e4d6969bebfdfe41454b3aa34e4ab
virus <1476374914@qq.com>
2021-11-13 15:46:45 +0800

    nc3

--- utils/after_strip_optimizer.py
+++ utils/after_strip_optimizer.py
@@ -1,4 +1,4 @@
 from utils.general import strip_optimizer
 
-strip_optimizer("/home/virus/DatasetWarehouse/exp_flex/exp-swinS-p2-2-31-map65.3-nc3-384/weights/last.pt",
-                "../weights/exp-swinS-p2-2-31-map65.3-nc3-384.pt")
+strip_optimizer("../weights/exp-swinT-p1-45-map68.5-384-adam-focal.pt",
+                "../weights/swinT.pt")
